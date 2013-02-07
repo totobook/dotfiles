7131a76aec862bf22b1d37d1522ef2062d501fc7
totobook <totobook2@gmail.com>
2013-02-07 23:13:30 +0900

    ruby-mode and tmux

---
+++ .emacs.d/conf/05-ruby.el
@@ -0,0 +1,21 @@
+;; カッコの自動挿入
+(require 'ruby-electric nil t)
+
+;; endに対応する行のハイライト
+(when (require 'ruby-block nil t)
+  (setq ruby-block-highlight-toggle t))
+
+;; インタラクティブRubyを利用する
+(autoload 'run-ruby "inf-ruby"
+  "Run an inferior Ruby prosess")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+
+;; ruby-mode-hookの関数を定義
+(defun ruby-mode-hooks ()
+  (inf-ruby-keys)
+  (ruby-electric-mode t)
+  (ruby-block-mode t))
+
+;; ruby-mode-hookに追加
+(add-hook 'ruby-mode-hook 'ruby-mode-hooks)
